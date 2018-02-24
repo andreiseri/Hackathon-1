6d826d51753bc06e5ac7199add6686731b7ceeb5
laur4444 <36791108+laur4444@users.noreply.github.com>
2018-02-24 14:44:01 +0200

    Initial commit

---
+++ app/src/main/java/net/ddns/dunno/hackathon/MainActivity.java
@@ -0,0 +1,19 @@
+package net.ddns.dunno.hackathon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Ceva();
+    }
+    private void Ceva()
+    {
+        int i = 1 + 1;
+        i = 1;
+    }
+}
